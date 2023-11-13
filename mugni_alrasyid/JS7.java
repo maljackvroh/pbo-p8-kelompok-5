84af4a279d45603c57c69ae6d78428f79b2c25f0
MugniAlRasyid <148302106+MugniAlRasyid@users.noreply.github.com>
2023-11-13 13:09:23 +0800

    Add files via upload

---
+++ mugni_alrasyid/JS7.java
@@ -0,0 +1,11 @@
+package js7;
+
+public class JS7 {
+    public static void main(String[] args) {
+        solarsystem data = new solarsystem();
+
+        data.display();
+        data.showE();
+        data.showM();
+    }
+}
