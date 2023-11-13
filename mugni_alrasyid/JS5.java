f779204944c28aaee33aa187f86fa73ac5b33d2c
MugniAlRasyid <148302106+MugniAlRasyid@users.noreply.github.com>
2023-11-13 13:06:55 +0800

    Add files via upload

---
+++ mugni_alrasyid/JS5.java
@@ -0,0 +1,10 @@
+package js5;
+
+public class JS5 {
+    public static void main(String[] args) {
+        makhlukHidup obj = new makhlukHidup();
+
+        obj.mthod();
+        obj.mtthod();
+    }
+}
